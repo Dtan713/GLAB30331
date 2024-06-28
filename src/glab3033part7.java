dbdc9396edf2005fe082baf0542001c2d3bae734
Dtan713 <s.donaldtan@gmail.com>
2024-06-28 12:35:43 -0400

    part7 is done

--- src/glab3033part7.java
+++ src/glab3033part7.java
@@ -1,9 +1,16 @@
+import java.util.Arrays;
+
 public class glab3033part7 {
     public static void main(String[] args) {
         String vowels = "a::b::c::d:e";
         // spitting the string at "::"
         // storing the results in an array of strings
+
         String[] result = vowels.split("::");
+
+        //converting array to string and printing it
+
+        System.out.println("result = " + Arrays.toString(result));
 
 
 
