74d884bd04e2b8641cadebe7842c70fc59ef5d25
Dtan713 <s.donaldtan@gmail.com>
2024-06-28 10:35:25 -0400

    adding glab30333

---
+++ src/glab30331part2.java
@@ -0,0 +1,12 @@
+public class glab30331part2 {
+    public static void main(String args[]) {
+
+
+    String s1="";
+    String s2="hello";
+        System.out.println(s1.isEmpty());  //true
+        System.out.println(s2.isEmpty());  //false
+
+
+
+}}
