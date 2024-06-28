64b23965dd5d2c60ef6076e2f3cb64267214dc2f
Dtan713 <s.donaldtan@gmail.com>
2024-06-28 12:29:46 -0400

    adding more of part 7

--- src/glab3033part7.java
+++ src/glab3033part7.java
@@ -1,6 +1,9 @@
 public class glab3033part7 {
     public static void main(String[] args) {
         String vowels = "a::b::c::d:e";
+        // spitting the string at "::"
+        // storing the results in an array of strings
+        String[] result = vowels.split("::");
 
 
 
