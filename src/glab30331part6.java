02fa642a537fbe5b1fd8199a950899a25afa6fab
Dtan713 <s.donaldtan@gmail.com>
2024-06-28 12:19:09 -0400

    part6

---
+++ src/glab30331part6.java
@@ -0,0 +1,10 @@
+public class glab30331part6 {
+    public static void main(String[] args) {
+        // ---- By using concat method ----
+        String str1 = "Learn";
+        String str2 = "Java";
+        // concatenate str1 and str2
+        System.out.println(str1.concat(str2)); //java
+
+    }
+}
