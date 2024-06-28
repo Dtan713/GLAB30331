55c76b9d1f36b49fefa42d3438240a380a0c2b44
Dtan713 <s.donaldtan@gmail.com>
2024-06-28 11:55:00 -0400

    part4

---
+++ src/glab30331part4.java
@@ -0,0 +1,9 @@
+public class glab30331part4 {
+        public static void main(String args[]){
+            String s1="HELLO HOW Are You?";
+            String s1lower=s1.toLowerCase();
+            System.out.println(s1lower);}
+    }
+
+
+
