3375ca0b9909f1e8a50f4161f9b88174bf47934e
Dtan713 <s.donaldtan@gmail.com>
2024-06-28 12:12:01 -0400

    part5

---
+++ src/glab30331part5.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+
+public class glab30331part5 {
+    public static void main(String[] args) {
+    String s1="hello how are you";
+    String s1upper=s1.toUpperCase();
+        System.out.println(s1upper);
+
+}}
