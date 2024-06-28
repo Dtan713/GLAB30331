e7db58e0b87d42cf39c2f66161864984f9ae38f7
Dtan713 <s.donaldtan@gmail.com>
2024-06-28 10:57:00 -0400

    glab30331 part3

---
+++ src/glab30331part3.java
@@ -0,0 +1,8 @@
+public class glab30331part3 {
+    public static void main(String args[]) {
+        String s1 = "  Hello    ";
+        System.out.println(s1+ "how are you"); //without trim()
+        System.out.println(s1.trim() + "how are you"); //with trim()
+
+    }
+}
